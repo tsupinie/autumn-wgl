0b24e10efc0542bc5c88a406db4d3b9186ab4efe
Tim Supinie <tsupinie@gmail.com>
2023-08-19 11:48:31 -0500

    Don't export screen buffer class

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@
 import {WGLBuffer} from "./WGLBuffer";
 import {WGLTexture} from "./WGLTexture";
 import {WGLProgram} from "./WGLProgram";
-import {WGLFramebuffer, WGLScreenbuffer, flipFlopBuffers} from "./WGLFramebuffer";
+import {WGLFramebuffer, flipFlopBuffers} from "./WGLFramebuffer";
 
 import type {WGLTextureSpec} from "./WGLTexture";
 
-export {WGLBuffer, WGLTexture, WGLTextureSpec, WGLProgram, WGLFramebuffer, WGLScreenbuffer, flipFlopBuffers};
+export {WGLBuffer, WGLTexture, WGLTextureSpec, WGLProgram, WGLFramebuffer, flipFlopBuffers};
